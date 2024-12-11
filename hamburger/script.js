64a10dfc2793a0ce53056b93c82943a53cc6360f
Marlon M. Sarabia Jr. <marlon.sarabia44@gmail.com>
2024-12-11 18:50:37 +0800

    shesh

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,11 +1,17 @@
 
 // Sir we admit nga this part ket nag base kamla kadejay internet para mapagbalin mila tay hamburger icon sir HUHU because our knowledge ket bassit pela sir HUHU
 
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
+
 
 menuOpenButton.addEventListener("click", () => {
     document.body.classList.toggle("show-mobile-menu");
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener ("click", ()  => menuOpenButton.click());
+})
