1abaad2793fd15444ef20189b7639ea3e1a9c0ec
Marlon M. Sarabia Jr. <marlon.sarabia44@gmail.com>
2025-05-01 09:52:19 +0800

    final

--- hamburger/script.js
+++ hamburger/script.js
@@ -2,15 +2,6 @@ const $navLinks = $(".nav-menu .nav-link");
 const $menuOpenButton = $("#menu-open-button");
 const $menuCloseButton = $("#menu-close-button");
 
-<<<<<<< HEAD
-=======
-// Sir we admit nga this part ket nag base kamla kadejay internet para mapagbalin mila tay hamburger icon sir HUHU because our knowledge ket bassit pela sir HUHU
-
-const $navLinks = $(".nav-menu .nav-link");
-const $menuOpenButton = $("#menu-open-button");
-const $menuCloseButton = $("#menu-close-button");
-
->>>>>>> 6faf49549e75f08371f07c502ba813f54fe56f20
 $menuOpenButton.on("click", function () {
     $("body").toggleClass("show-mobile-menu");
 });
