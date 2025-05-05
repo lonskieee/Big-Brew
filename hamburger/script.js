06d0e92fa75ecb99d91b17765c1b6a7951a83e02
Marlon M. Sarabia Jr. <marlon.sarabia44@gmail.com>
2025-05-05 20:46:49 +0800

    deploy

--- hamburger/script.js
+++ hamburger/script.js
@@ -13,3 +13,31 @@ $menuCloseButton.on("click", function () {
 $navLinks.on("click", function () {
     $menuOpenButton.click();
 });
+
+$(document).ready(function () {
+
+    $(".menu-img").hide(); // Hides all images by default
+
+    // 🔁 Set initial button text based on image visibility
+    $(".toggle-btn").each(function () {
+        const img = $(this).siblings(".menu-img");
+        if (img.is(":visible")) {
+            $(this).text("〤");
+        } else {
+            $(this).text("☕︎");
+        }
+    });
+
+    // 🔁 Toggle image and button text on click
+    $(".toggle-btn").click(function () {
+        const img = $(this).siblings(".menu-img");
+        img.slideToggle(400, () => {
+            // callback to make sure the text updates after animation
+            if (img.is(":visible")) {
+                $(this).text("〤");
+            } else {
+                $(this).text("☕︎");
+            }
+        });
+    });
+});
